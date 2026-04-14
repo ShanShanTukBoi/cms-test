fb705402a0fa259c32f02159fb3b90147cbf115b
ShanShanTukBoi <rushantucker@hotmail.com>
2026-04-14 17:49:28 +1000

    Change oauth provider

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const { createOAuthHandler } = require("netlify-cms-oauth-provider");
+const { createOAuthHandler } = require("@decap-cms/oauth-provider-node");
 
 exports.handler = createOAuthHandler({
   site_id: process.env.SITE_ID,
